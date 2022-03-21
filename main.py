63693798cb6ad4c41b1a7c503fd9d5fa202c07ac
Vitor Justamand <justamandd@gmail.com>
2022-03-20 21:59:50 -0300

    Update: Filtering input data

--- main.py
+++ main.py
@@ -2,11 +2,27 @@
 
 import math
 
-binary = input('Type a binary string with lenght between 1 to 8 characters:\n')
+while True:
+    binary = input('Type a binary string with lenght between 1 to 8 characters: ')
 
-decimal = 0
+    binary.split()
 
-for i in range(len(binary)):
-    decimal += int(binary[i]) * pow(2, int(len(binary)) - i - 1)
+    isBinary = False
 
-print(f'Your binary number is: {decimal}')
+    for i in range(len(binary)):
+        if binary[i] == '1' or binary[i] == '0':
+            isBinary = True
+        else:
+            isBinary = False
+            print('You can just type 0 or 1')
+            break
+
+    if isBinary == True:
+        decimal = 0
+        for i in range(len(binary)):
+            decimal += int(binary[i]) * pow(2, int(len(binary)) - i - 1)
+
+        print(f'Your binary number is: {decimal}')
+
+    else:
+        continue
